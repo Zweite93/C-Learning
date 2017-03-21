5a095de9eb06a3d7561d0394fabf18f9bf1c6a8d
Zweite93 <zweite93@gmail.com>
2017-03-21 16:46:34 +0200

    Sort at Add

---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+            list.Add(4);
+            list.Add(1);
+            list.Add(3);
+            list.Add(2);
+            list.Add(-1);
+
+            list.Display();
+        }
+    }
+
+    class MyList
+    {
+        private List<int> _list;
+
+        public MyList()
+        {
+            _list = new List<int>();
+        }
+
+        public void Add(int value)
+        {
+            int index = (_list.FindLastIndex(r => r <= value));
+            index++;
+            _list.Insert(index, value);
+        }
+
+        public void Display()
+        {
+            foreach (var item in _list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
