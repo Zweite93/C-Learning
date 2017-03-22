5e3bea2e13495c9b12cd7f67bdddaa6659d75806
Zweite93 <zweite93@gmail.com>
2017-03-22 23:54:16 +0200

    Search

---
+++ Search/Search/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Search("12 3abc123a1", "a123"));
+        }
+
+        static bool Search(string stringToSearch, string word)
+        {
+            char[] chars = word.ToCharArray();
+            char firstWordChar = chars[0];
+
+            if (String.Equals(stringToSearch, word))
+                return true;
+
+            for (int stringIndex = 0; stringIndex < stringToSearch.Length; stringIndex++)
+            {
+                if ((stringToSearch.Length - stringIndex) < chars.Length)
+                    return false;
+
+                if (stringToSearch[stringIndex] == firstWordChar)
+                {
+                    int secondStringIndex = stringIndex;
+
+                    for (int charsIndex = 1; charsIndex < chars.Length; charsIndex++)
+                    {
+                        secondStringIndex++;
+                        if (stringToSearch[secondStringIndex] == chars[charsIndex])
+                            if (charsIndex == chars.Length - 1)
+                                return true;
+                            else
+                                continue;
+                        else
+                            break;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
